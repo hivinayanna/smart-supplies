4ca29e2f3819642a8682c4eeec74150e64f3ff5c
rudha <rudhaesmeraldo@gmail.com>
2025-04-09 18:04:55 -0300

    ➕ feat: Criação de rotas para autenticação via JWT no urls.py

--- src/BackEnd/smart_supplies/urls.py
+++ src/BackEnd/smart_supplies/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')), #incluindo as rotas para a api
+
+    # JWT - Login e refresh
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
