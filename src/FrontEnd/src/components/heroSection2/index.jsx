fa9c6ea198dd721da9483cac998d2820fc99869a
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-04-30 00:09:41 +0000

    💄 feat: Componente HeroSection2

---
+++ src/FrontEnd/src/components/heroSection2/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../styles/heroSection2.css';
+
+
+
+const HeroSection2 = () => {
+    return (
+        
+        <div>
+
+        </div>
+    );
+};
+
+export default HeroSection2;
