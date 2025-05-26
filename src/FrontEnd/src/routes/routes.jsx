3ad3585c2d374150fdda7d2ab8af3e5e7aeffe3f
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-05-26 15:25:21 -0300

    ✨ feat: Adicionando Rota de Auth

--- src/FrontEnd/src/routes/routes.jsx
+++ src/FrontEnd/src/routes/routes.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Auth from "../pages/Auth";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Auth" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   );
