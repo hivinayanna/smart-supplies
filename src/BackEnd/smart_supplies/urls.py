3b2611f7bc338801d8eaef81755cd677129358ea
rudha <rudhaesmeraldo@gmail.com>
2025-06-10 18:23:43 -0300

    ✨ feat: Servir arquivos de mídia com static() em ambiente de desenvolvimento

--- src/BackEnd/smart_supplies/urls.py
+++ src/BackEnd/smart_supplies/urls.py
@@ -4,6 +4,8 @@ from rest_framework_simplejwt.views import(
     TokenObtainPairView, # View para obtenção do token JWT (login)
     TokenRefreshView # View para renovação do token JWT (refresh)
 )
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls), # URL padrão do admin
@@ -12,4 +14,4 @@ urlpatterns = [
     # Endpoints para autenticação via JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
